fc3f1fcf94468ab7936216aa7dc10423f168f8c6
Matteo Dessalvi <m.dessalvi@gsi.de>
2014-03-25 11:46:41 +0100

    First draft of the default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,31 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+package "redis"
+
+user node[:redis][:user] do
+  home node[:redis][:data_dir]
+  system true
+end
+
+directory node[:redis][:config] do
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+directory node[:redis][:db_dir] do
+  owner node[:redis][:user]
+  mode "0750"
+end
+
+service "redis" do
+  supports :reload => false, :restart => true, :start => true, :stop => true
+  action [ :enable, :start ]
+end
+
+template "#{node[:redis][:config]}" do
+  source "redis.conf.erb"
+  mode "0644"
+  notifies :restart, "service[redis]"
+end
