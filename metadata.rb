bf8ebb46172dbc1e49411c68f6d881cd3ec02825
Matteo Dessalvi <m.dessalvi@gsi.de>
2014-09-04 16:36:43 +0200

    Version bumped

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "hpc@gsi.de"
 license          "All rights reserved"
 description      "Installs/Configures redis"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.2"
+version          "0.1.3"
