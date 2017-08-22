10a46e301fbd72cf476068e4c43541aa9209c1c8
Matteo Dessalvi <m.dessalvi@gsi.de>
2017-08-22 15:02:01 +0200

    Updated metadata: new source_url

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ license          "All rights reserved"
 description      "Installs/Configures redis"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.7"
-source_url       'https://gitorious.gsi.de/site-cookbooks/redis'
+source_url       'https://git.gsi.de/chef/cookbooks/redis'
